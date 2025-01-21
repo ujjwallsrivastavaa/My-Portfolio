d62f178009633db422936e9e59c2934e9b366582
ujjwallsrivastavaa <2003ujjwalusri@gmail.com>
2025-01-22 04:22:24 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
-import { FaCode, FaMoon, FaSun } from "react-icons/fa";
+import { FaMoon, FaSun, FaHome, FaFolder, FaLaptopCode, FaCommentDots } from "react-icons/fa";
+
 
 function Header({ homeRef, skillsRef, contactRef, projectsRef, toggleDarkMode, isDarkMode }) {
   const [hasShadow, setHasShadow] = useState(false);
   const [activeSection, setActiveSection] = useState('');
 
   const sectionRefs = [
-    { section: 'HOME', ref: homeRef },
-    { section: 'PROJECTS', ref: projectsRef },
-    { section: 'SKILLS', ref: skillsRef },
-    { section: 'CONTACT', ref: contactRef },
+    { section: 'Home', ref: homeRef, icon: <FaHome /> },
+    { section: 'Projects', ref: projectsRef, icon: <FaFolder /> },
+    { section: 'Skills', ref: skillsRef, icon: <FaLaptopCode /> },
+    { section: 'Contact', ref: contactRef, icon: <FaCommentDots /> },
   ];
 
   const scrollToSection = (sectionRef) => {
@@ -69,37 +70,45 @@ function Header({ homeRef, skillsRef, contactRef, projectsRef, toggleDarkMode, i
   }, []);
 
   return (
+
     <div
-      className={`fixed top-0 left-0 right-0 ${
-        isDarkMode ? 'bg-black text-white' : 'bg-white text-black'
-      } ${hasShadow ? "border-b-[2px] shadow-lg" : ""} transition-all duration-300 py-5 flex justify-between items-center px-6`}
+      className={`fixed top-0 left-0 right-0 ${isDarkMode ? 'bg-black text-white' : 'bg-white text-black'
+        } ${hasShadow ? "border-b-[2px] shadow-lg" : ""} transition-all duration-300 py-5 flex justify-between items-center px-6`}
     >
-      <FaCode />
+      {/* <FaCode /> */}
       <nav>
         <ul className="flex justify-center items-center gap-10">
-          {sectionRefs.map(({ section, ref }) => (
+          {sectionRefs.map(({ section, ref, icon }) => (
             <li
               key={section}
-              className={`cursor-pointer relative inline-block group hover:font-semibold ${
-                activeSection === section ? "font-bold" : ""
-              } transition-all duration-200`}
+              className={`cursor-pointer relative inline-block group hover:font-semibold ${activeSection === section ? "font-bold" : ""
+                } transition-all duration-200`}
               onClick={() => scrollToSection(ref)}
             >
-              {section}
+              <div className="flex items-center justify-center">
+                <div className="text-4xl"> {/* Adjust size here, e.g., text-3xl for larger icons */}
+                  {icon} {/* This will render the icon */}
+                </div>
+              </div>
+
+              {/* Tooltip for section name */}
               <span
-                className={`absolute bottom-0 left-[2px] right-[2px] h-0.5 ${
-                  isDarkMode ? 'bg-white' : 'bg-black'
-                } transform ${
-                  activeSection === section
+                className={`absolute top-full left-1/2 transform -translate-x-1/2 text-sm mt-1 px-2 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200`}
+              >
+                {section} {/* Section name (HOME, SKILLS, etc.) */}
+              </span>
+              <span
+                className={`absolute bottom-0 left-[2px] right-[2px] h-0.5 ${isDarkMode ? 'bg-white' : 'bg-black'
+                  } transform ${activeSection === section
                     ? "translate-y-0 opacity-100"
                     : "-translate-y-1 opacity-0"
-                } transition-all duration-200`}
+                  } transition-all duration-200`}
               />
             </li>
           ))}
         </ul>
       </nav>
-      <button onClick={toggleDarkMode} className="text-2xl ">
+      <button onClick={toggleDarkMode} className="text-5xl ">
         {isDarkMode ? <FaSun /> : <FaMoon />}
       </button>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import usdp from "../utils/dp.jpg"; // Ensure the correct path to your image file
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Hero = ({ homeRef, projectsRef, contactRef }) => {
   const scrollToSection = (sectionRef) => {
@@ -13,40 +15,63 @@ const Hero = ({ homeRef, projectsRef, contactRef }) => {
       ref={homeRef}
       className="flex items-center flex-wrap justify-center gap-20 px-10 py-36"
     >
-      <div className="max-w-[600px] flex flex-col gap-5 flex-grow">
+      <div className="max-w-[650px] flex flex-col gap-5 flex-grow">
         <h1 className="text-5xl font-bold ">Ujjwal Srivastava</h1>
         <h3 className="text-2xl lg:text-3xl text-gray-500">
           Software Developer
         </h3>
         <p className="lg:text-xl">
-          A versatile software developer skilled in React, Node.js, Python,
-          Java, and C++, with a passion for building dynamic and efficient
-          applications.
+          An innovative software developer proficient in React, Node.js, Python, Java, C++, and Machine Learning, with a strong passion for problem-solving and building scalable, high-performance applications.
         </p>
         <div className="flex gap-10">
+
           <button
-            className="lg:text-lg py-2 px-4 bg-black text-white rounded-lg hover:opacity-70 transition-all duration-150"
-            onClick={() => scrollToSection(projectsRef)}
+            className="lg:text-lg py-2 px-4 border-[1px] border-white bg-black text-white rounded-lg hover:opacity-80 transition-all duration-150"
+            onClick={() => {
+              window.open("https://drive.google.com/file/d/1CPozQT-banJxR4sixpqbSlMEjn5htnTT/view?usp=sharing", "_blank"); // The file is now accessible from the root of your site
+            }}
           >
-            View Projects
+            View Resume
           </button>
+
           <button
-            className="lg:text-lg py-2 px-4 border-[1px] border-gray-200 rounded-lg hover:bg-muted transition-all duration-150"
+            className="lg:text-lg py-2 px-4 border-[1px] border-white bg-black text-white rounded-lg hover:opacity-80 transition-all duration-150"
             onClick={() => scrollToSection(contactRef)}
           >
             Contact Me
           </button>
+
+
+          <button
+            className="text-4xl"
+            onClick={() => {
+              window.open("https://instagram.com/ujjwallsrivastavaa", "_blank");
+            }}
+          >
+            <FaLinkedinIn />
+          </button>
+
+          <button
+            className="text-4xl"
+            onClick={() => {
+              window.open("https://instagram.com/ujjwallsrivastavaa", "_blank");
+            }}
+          >
+            <FaGithub />
+          </button>
+
         </div>
+
       </div>
 
       {/* Replaced About Me with Image */}
       <div className="max-w-[400px] flex-grow flex justify-center">
-  <img
-    src={usdp} // Now using the imported image
-    alt="Ujjwal Srivastava"
-    className="w-auto h-auto rounded-full object-cover border-2 border-black"
-  />
-</div>
+        <img
+          src={usdp} // Now using the imported image
+          alt="Ujjwal Srivastava"
+          className="w-auto h-auto rounded-full object-cover border-2 border-black"
+        />
+      </div>
 
     </div>
   );
